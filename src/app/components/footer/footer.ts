efc1a408fae210e2e7c5c46a210e30be47a23437
Nicolas Mezieres <nicolas.mezieres73@gmail.com>
2025-07-18 01:28:12 +0200

    remove html and css file footer, move html footer in template footer.ts

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  template: `<footer
+    class="flex gap-10 justify-center py-5 border-t border-white text-white font-agdasima xl:text-xl"
+  >
+    <p>Legal notice</p>
+    <p>Copyright</p>
+  </footer> `,
 })
-export class Footer {
-
-}
+export class Footer {}
