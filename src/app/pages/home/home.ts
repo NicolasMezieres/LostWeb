253605dd0f61585e64d4d608492abb5c813d2417
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-07-25 19:21:29 +0200

    page home finish with responsive

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { Header } from "../../components/header/header";
-import { Footer } from "../../components/footer/footer";
+import { Header } from '../../components/header/header';
+import { Footer } from '../../components/footer/footer';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Footer],
-  templateUrl: './home.html',
-  styleUrl: './home.css'
+  imports: [Header, Footer, MatButton, RouterLink],
+  template: `<div class="flex flex-col min-h-screen bgGradient">
+    <app-header [index]="1"></app-header>
+    <main
+      class="flex-auto flex flex-col justify-center items-center gap-10 md:gap-20 text-center text-white px-5 md:px-10 xl:px-20"
+    >
+      <h1 class="font-kay text-xl md:text-[32px]">
+        Welcome to the panel Aadministrator
+      </h1>
+      <h2 class="font-agdasima text-xl md:text-2xl">
+        Use the menu for navigate to the different page and access to the data
+        of the main app
+      </h2>
+      <ul class="text-black flex flex-col gap-10 justify-center md:flex-row">
+        <a matButton="outlined" [routerLink]="'/user_list'">User list</a>
+        <a matButton="outlined" [routerLink]="'/announcement_list'"
+          >Announcement list</a
+        >
+        <a matButton="outlined" class="border" [routerLink]="'/statistic'"
+          >Statistic</a
+        >
+      </ul>
+    </main>
+    <app-footer></app-footer>
+  </div> `,
 })
-export class Home {
-
-}
+export class Home {}
